48591dad85bc73900df1c67c26af265d7e720fa7
krishipyy <shresthakrishiv10@gmail.com>
2026-05-03 19:50:06 +0545

    L01 HW

---
+++ L01/L01 HW My Daily Routine Printer.py
@@ -0,0 +1,20 @@
+# My Daily Routine
+print("I wake up at 6:30 AM.")
+
+print("I brush my teeth at 6:45 AM.")
+
+print("I get ready for school  at 7:00 AM.")
+
+print("I study for 30 minutes at 7:15 AM.")
+
+print("I eat breakfast at 7:50 AM or sometimes 8:00 AM ")
+
+print("I go to school at 8:30 AM.")
+
+print("I come home at 4:40 PM.")
+
+print("I do my homework at 5:30 PM.")
+
+print("I eat dinner at 7:30 PM.")
+
+print("I go to bed at 11:00 PM.")
