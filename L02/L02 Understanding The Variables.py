25565a84b2d5d9e139c612a064bd14680c8f01fc
krishipyy <shresthakrishiv10@gmail.com>
2026-05-08 19:23:10 +0545

    L02

---
+++ L02/L02 Understanding The Variables.py
@@ -0,0 +1,20 @@
+#creating different variables
+x=5
+y="John"
+z=7.3
+
+print(x)
+print(y)
+print(z)
+
+#print(x+y) #it will give an error
+
+word="Codingal"
+print(word)
+
+#User input
+name=input("What is your name? ")
+print("Hello"+name,"Welcome to Codingal")
+      
+age=int(input("What is your age? "))
+print(type(age))
---
+++ L02/L02 Welcome To the World of Progamming.py
@@ -0,0 +1,15 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+print(4)
+
+#Next Line after print 
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("Hello",5)
+
+#End arguement of Print Statement
+print("Welcome to ", end="")
+print("test")
