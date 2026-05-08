2dc51a0c65788c0b518efe8e25cd42752da9bb20
krishipyy <shresthakrishiv10@gmail.com>
2026-05-08 20:04:44 +0545

    L02 h.w

--- L02/L02 Hw Storing Birthdate.py
+++ L02/L02 Hw Storing Birthdate.py
@@ -1,14 +1,14 @@
 #taking input for birtday 
-birthday1=input("When is your Birtdhay,Jack?")
-birthday2=input("When is your Birtdhay,Shawn?")
-birthday3=input("When is your Birtdhay,Peter?")
-birthday4=input("When is your Birtdhay,Aaron?")
-birthday5=input("When is your Birtdhay,John?")
+birthday1=input("When is your Birtday,Jack?")
+birthday2=input("When is your Birtday,Shawn?")
+birthday3=input("When is your Birtday,Peter?")
+birthday4=input("When is your Birtday,Aaron?")
+birthday5=input("When is your Birtday,John?")
 
 #printing the birthday
-print("So,Jack's birthday is on" birthday1)
-print("So,Shawn's birthday is on" +birthday2)
-print("So,Peter's birthday is on" +birthday3)
-print("So,Aaron's birthday is on" +birthday4)
-print("So,John's birthday is on" +birthday5)
+print("So,Jack your birthday is on" + birthday1)
+print("So,Shawn your birthday is on" + birthday2)
+print("So,Peter your birthday is on" + birthday3)
+print("So,Aaron your birthday is on" + birthday4)
+print("So,John your birthday is on" + birthday5)
 
