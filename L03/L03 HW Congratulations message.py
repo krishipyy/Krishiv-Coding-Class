23d587ece49bea21a0aba7f58e72e1b801e77fb9
krishipyy <shresthakrishiv10@gmail.com>
2026-05-15 20:30:23 +0545

    L03 H.W

---
+++ L03/L03 HW Congratulations message.py
@@ -0,0 +1,11 @@
+word= "CONGRATULATIONS"
+low=word.lower()
+print(low)
+
+word= "best of luck"
+upper=word.upper()
+print(upper)
+
+#something extra
+name=input("Enter your name: ")
+print("Congratulations", name)
