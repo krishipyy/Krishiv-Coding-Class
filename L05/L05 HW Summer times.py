d6e43ccc666c071c007049b9db267291818ac437
krishipyy <shresthakrishiv10@gmail.com>
2026-05-26 20:24:00 +0545

    L05 H.W

---
+++ L05/L05 HW Summer times.py
@@ -0,0 +1,7 @@
+temp = int(input("Enter the temperature in degree Celsius: "))
+
+if temp < 25:
+    print("It is a cold day, wear jacket or pullover" )
+else:
+    print("It is a hot day, wear light clothes")
+    
