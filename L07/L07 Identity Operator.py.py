0b13f8b8b5617a9edd1ac3243ca7a6d922db6da2
krishipyy <shresthakrishiv10@gmail.com>
2026-06-02 19:21:04 +0545

    L06

---
+++ L07/L07 Identity Operator.py.py
@@ -0,0 +1,23 @@
+#python program to illustrate the use 
+# of 'is' identity operator
+x=5
+if (type(x) is int):
+    print("True")
+else:
+    print("False")
+
+x=5.0
+if(type(x) is not float):
+    print("True")
+else:
+    print("False")
+
+x=20
+y=20
+if(x is y):
+    print("x and y have same identity")
+
+x=20
+y=30
+if(x is not y):
+    print("x and y have different identity")
