5191ac3bf261f71b214ca8070eed99e5b71fc67c
krishipyy <shresthakrishiv10@gmail.com>
2026-06-05 18:17:55 +0545

    L07 H.W

---
+++ L07/L07 HW ASCII value.py
@@ -0,0 +1,35 @@
+c = input("enter any character: ")
+
+if c == "A":
+    print("the ASCII code is 65")
+elif c == "B":
+    print("the ASCII code is 66")
+elif c == "B":
+    print("the ASCII code is 67")
+elif c == "C":
+    print("the ASCII code is 68")
+elif c == "D":
+    print("the ASCII code is 69")
+elif c == "E":
+    print("the ASCII code is 70")
+elif c == "F":
+    print("the ASCII code is 71")
+elif c == "G":
+    print("the ASCII code is 72")
+
+
+
+elif c == "a":
+    print("the ASCII code is 97")
+elif c == "b":
+    print("the ASCII code is 98")
+elif c == "c":
+    print("the ASCII code is 99")
+elif c == "d":
+    print("the ASCII code is 100")
+elif c == "e":
+    print("the ASCII code is 101")
+elif c == "f":
+    print("the ASCII code is 102")
+elif c == "g":
+    print("the ASCII code is 103")
